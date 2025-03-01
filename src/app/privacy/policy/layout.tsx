046af4fe8a7fde22e4230f6d43c6df9adbc44d64
opeCre8tivia <opegude.n3t@gmail.com>
2025-03-01 14:58:41 +0300

    fix:import

--- src/app/privacy/policy/layout.tsx
+++ src/app/privacy/policy/layout.tsx
@@ -1,9 +1,9 @@
 
-import "./globals.css"
-import { Inter } from "next/font/google"
+import "../../globals.css"
+// import { Inter } from "next/font/google"
 import { Metadata } from "next"
 
-const inter = Inter({ subsets: ["latin"] })
+// const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Privacy Policy",
